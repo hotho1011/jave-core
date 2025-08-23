bca18f2e3abafbb58687e0f9c44ac91789ef19b5
hothob3 <hothob3@gmail.com>
2025-08-23 21:56:09 +0700

    update bai tap lab 01

--- java-core/src/chapter2/lab01bai2.java
+++ java-core/src/chapter2/lab01bai2.java
@@ -5,6 +5,16 @@ import java.util.Scanner;
 public class lab01bai2 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		System.out.print("Nhập chiều dài: ");
+		int chieu_dai = scanner.nextInt();
+		System.out.print("Nhập chiều rộng: ");
+		int chieu_rong = scanner.nextInt();
+		int chu_vi = (chieu_dai + chieu_rong)*2;
+		int dien_tich = chieu_dai * chieu_rong;
+		int min = Math.min(chieu_dai, chieu_rong);
+		System.out.println("Chu vi hinh chu nhat là: " +chu_vi);
+		System.out.println("Dien tich hinh chu nhat là: " +dien_tich);
+		System.out.println("cạnh nhỏ nhất là: " +min);
 		
 	}
 }
---
+++ java-core/src/chapter2/lab01bai3.java
@@ -0,0 +1,18 @@
+package chapter2;
+
+import java.util.Scanner;
+
+public class lab01bai3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Nhập vào cảnh của một khối lập phương, tính và xuất ra thể tích của khối lập phương
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Nhập cạnh của khối lập phương: ");
+		int a = scanner.nextInt();
+		int the_tich = (int)Math.pow(a, 3);
+		System.out.print("Thể tích của khối lập phương là: "+ the_tich);
+	}
+
+}
